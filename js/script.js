eb061bcb8483e60029c281137f05d554e3ceefe5
kallew-dev <kmfp1206@gmail.com>
2026-08-04 01:02:53 -0300

    feat: add typewriter animation ultilities

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const element = document.getElementById("typewrite");
+
 const NAME = "Kauã Miranda";
 const ROLE = "Backend Developer";
 
@@ -6,3 +8,23 @@ const DELETE_SPEED = 35;
 
 const NAME_DELAY = 3000;
 const ROLE_DELAY = 25000;
+
+
+// Definir funcao de espera:
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+// Escrever na tela:
+async function typeText(text) {
+  element.textContent = "";
+
+  for (const letter of text) {
+    element.textContent += letter;
+
+    await sleep(TYPE_SPEED);
+  }
+}
+
+
+
